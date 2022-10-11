dcec8b4828a2b20b5e89a6e85b2f130787c12ec3
feisalmombo29 <feisalmombo29@gmail.com>
2022-10-11 15:06:49 +0300

    update login file

--- resources/views/layouts/login_master.blade.php
+++ resources/views/layouts/login_master.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'CJ INSPIRED ACADEMY') }}</title>
+    <title>{{ config('app.name', 'STUDENT MANAGEMENT SYSTEM') }}</title>
 
     @include('partials.login.inc_top')
 </head>
--- resources/views/partials/login/footer.blade.php
+++ resources/views/partials/login/footer.blade.php
@@ -8,12 +8,12 @@
 
     <div class="navbar-collapse collapse" id="navbar-footer">
 					<span class="navbar-text">
-						&copy; {{ date('Y') }}. <a href="#">{{ Qs::getSystemName() }}</a> by <a href="#" >CJ Inspired</a>
+						&copy; {{ date('Y') }}. <a href="#">{{ Qs::getSystemName() }}</a> by <a href="#" >EFE TECH</a>
 					</span>
 
         <ul class="navbar-nav ml-lg-auto">
-            <li class="nav-item"><a href="{{ route('privacy_policy') }}" class="navbar-nav-link" target="_blank"><i class="icon-lifebuoy mr-2"></i> Privacy Policy</a></li>
-            <li class="nav-item"><a href="{{ route('terms_of_use') }}" class="navbar-nav-link" target="_blank"><i class="icon-file-text2 mr-2"></i> Terms of Use </a></li>
+            {{--  <li class="nav-item"><a href="{{ route('privacy_policy') }}" class="navbar-nav-link" target="_blank"><i class="icon-lifebuoy mr-2"></i> Privacy Policy</a></li>  --}}
+            {{--  <li class="nav-item"><a href="{{ route('terms_of_use') }}" class="navbar-nav-link" target="_blank"><i class="icon-file-text2 mr-2"></i> Terms of Use </a></li>  --}}
 {{--            <li class="nav-item"><a href="#" class="navbar-nav-link font-weight-semibold"><span class="text-pink-400"><i class="icon-phone mr-2"></i> Contact Us</span></a></li>--}}
         </ul>
     </div>
