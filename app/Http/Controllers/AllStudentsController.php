3ebdbf25d2691f47aa30236dfa4cb926649393a9
feisalmombo29 <feisalmombo29@gmail.com>
2022-10-21 16:19:50 +0300

    WIP

--- app/Http/Controllers/AllStudentsController.php
+++ app/Http/Controllers/AllStudentsController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
+use Auth;
 
 class AllStudentsController extends Controller
 {
-    //
+    public function getAllStudents()
+    {
+        $auth_super = Auth::user()->id;
+
+        $allstudents = DB::table('users')->where('user_type', '=', 'student');
+    }
 }
