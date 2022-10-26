cffac200e765822c1d1d925f0498df2c9783e263
feisalmombo29 <feisalmombo29@gmail.com>
2022-10-26 16:41:21 +0300

    WIP

--- resources/views/pages/support_team/dashboard.blade.php
+++ resources/views/pages/support_team/dashboard.blade.php
@@ -77,12 +77,12 @@
     @if(Qs::userIsTeamSA())
         <div class="row">
             <div class="col-sm-6 col-xl-3">
-                <a href="{{ url('/all/students') }}">
+                <a href="#">
                     <div class="card card-body bg-blue-400 has-bg-image">
                         <div class="media">
                             <div class="media-body">
-                                <h3 class="mb-0">{{ $users->where('user_type', 'student')->count() }}</h3>
-                                <span class="text-uppercase font-size-xs font-weight-bold">Total Students</span>
+                                <h3 class="mb-0">34</h3>
+                                <span class="text-uppercase font-size-xs font-weight-bold">Total Subjects</span>
                             </div>
 
                             <div class="ml-3 align-self-center">
@@ -94,12 +94,12 @@
             </div>
 
             <div class="col-sm-6 col-xl-3">
-                <a href="{{ url('/all/teachers') }}">
+                <a href="#">
                     <div class="card card-body bg-danger-400 has-bg-image">
                         <div class="media">
                             <div class="media-body">
-                                <h3 class="mb-0">{{ $users->where('user_type', 'teacher')->count() }}</h3>
-                                <span class="text-uppercase font-size-xs">Total Teachers</span>
+                                <h3 class="mb-0">56</h3>
+                                <span class="text-uppercase font-size-xs">Total Classes</span>
                             </div>
 
                             <div class="ml-3 align-self-center">
@@ -111,7 +111,7 @@
             </div>
 
             <div class="col-sm-6 col-xl-3">
-                <a href="{{ url('/all/administrators') }}">
+                <a href="#">
                     <div class="card card-body bg-success-400 has-bg-image">
                         <div class="media">
                             <div class="mr-3 align-self-center">
@@ -119,8 +119,8 @@
                             </div>
 
                             <div class="media-body text-right">
-                                <h3 class="mb-0">{{ $users->where('user_type', 'admin')->count() }}</h3>
-                                <span class="text-uppercase font-size-xs">Total Administrators</span>
+                                <h3 class="mb-0">40</h3>
+                                <span class="text-uppercase font-size-xs">Total Dormitories</span>
                             </div>
                         </div>
                     </div>
@@ -128,7 +128,7 @@
             </div>
 
             <div class="col-sm-6 col-xl-3">
-                <a href="{{ url('/all/parents') }}">
+                <a href="#">
                     <div class="card card-body bg-indigo-400 has-bg-image">
                         <div class="media">
                             <div class="mr-3 align-self-center">
@@ -136,8 +136,8 @@
                             </div>
 
                             <div class="media-body text-right">
-                                <h3 class="mb-0">{{ $users->where('user_type', 'parent')->count() }}</h3>
-                                <span class="text-uppercase font-size-xs">Total Parents</span>
+                                <h3 class="mb-0">700</h3>
+                                <span class="text-uppercase font-size-xs">Total Subjects</span>
                             </div>
                         </div>
                     </div>
