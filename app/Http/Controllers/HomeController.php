2920c2b1b27a269332db1180e90792b9b39606f6
feisalmombo29 <feisalmombo29@gmail.com>
2022-11-07 15:31:11 +0300

    WIP

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -45,6 +45,7 @@ class HomeController extends Controller
         }
 
         $allStudent = DB::table('users')->where('user_type', '=', 'student')->count();
+        // dd($allStudent);
 
         // pass all students information here
 
