21e7a18a215d9714c41c438f018a7ecbbc831a9b
feisalmombo29 <feisalmombo29@gmail.com>
2022-11-08 13:49:36 +0300

    WIP

--- resources/views/pages/administrators_all/administrators.blade.php
+++ resources/views/pages/administrators_all/administrators.blade.php
@@ -41,7 +41,7 @@
                 @else
                 <div class="alert alert-info">
                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                    <strong>No Teachers found</strong>
+                    <strong>No Administrators found</strong>
                 </div>
             @endif
             </div>
