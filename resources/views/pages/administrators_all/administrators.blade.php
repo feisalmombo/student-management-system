6a2e9a19286b04f42606273300de052b0faf1679
feisalmombo29 <feisalmombo29@gmail.com>
2022-11-08 13:48:26 +0300

    WIP

--- resources/views/pages/administrators_all/administrators.blade.php
+++ resources/views/pages/administrators_all/administrators.blade.php
@@ -38,6 +38,11 @@
                     @endforeach
                     </tbody>
                 </table>
+                @else
+                <div class="alert alert-info">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    <strong>No Teachers found</strong>
+                </div>
             @endif
             </div>
         </div>
